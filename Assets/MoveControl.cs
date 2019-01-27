5ea08f30c596fc5857bd7e5ed36057e524abf6d2
Rain Zhao <rainzhao2000@gmail.com>
2019-01-27 01:41:01 -0500

    test

---
+++ Assets/MoveControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl : MonoBehaviour
+{
+    public float acceleration;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * acceleration;
+        float moveVertical = Input.GetAxis("Vertical") * acceleration;
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
