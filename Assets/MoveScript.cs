79ce8e210879cea34e2cde7ad4308db1de23f83a
Rain Zhao <rainzhao2000@gmail.com>
2019-01-26 19:27:46 -0500

    adding zombie

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public double speed;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown("left"))
+        {
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rb.AddForce(movement * speed);
+        }
+    }
+}
